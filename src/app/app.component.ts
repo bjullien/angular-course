e363aa9b40dc780a8b97b9d621d7c6e8e052c659
Bruno Jullien <bruno.jullien@gmail.com>
2019-12-25 11:43:24 -0800

    6. key features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   data = {
-    title : 'Angular Core Deep Dive'
+    title : '<h1>Angular Core Deep Dive</h1><script>alert("Attack!")</script>'
   };
+  /*
+    SECURITY NOTE:
+      - if we add html, such as <h1> ... </h1> it will be escaped
+        automatically, and <h1> will be treated as text, not HTML
+      - if a hacker inject some <script></script>, <script> is escaped too
+  */
+
+  onLogoClicked() {
+    alert('Hello World');
+  }
+
+  onKeyUp(newTitle: string) {
+    // angular will reflect data changes into the view, automatically
+    // because of '<input> [value]="data.title"'
+    this.data.title = newTitle;
+  }
 }
